3f95fdd487d1890b2ad687959f84b7acbed3c735
Feedbaek <nicek789@naver.com>
2023-11-28 00:52:56 +0900

    Design: 헤더 nav 로그아웃 추가

--- src/main/java/seoul/AutoEveryDay/controller/HomeController.java
+++ src/main/java/seoul/AutoEveryDay/controller/HomeController.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Slf4j(topic = "HomeController")
 @Controller
@@ -13,12 +12,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/home")
 public class HomeController {
     @GetMapping("")
-    @ResponseBody
     public String home() {
-        return "<!DOCTYPE html>" +
-                "<html>" +
-                "<head><title>홈 화면</title></head>" +
-                "<body>안녕하세요. 홈 화면입니다.</body>" +
-                "</html>";
+        return "home";
     }
 }
